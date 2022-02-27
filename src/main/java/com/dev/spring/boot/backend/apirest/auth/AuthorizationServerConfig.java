c5b09aedfc703ac4ae0ab13116412d89b3eb4b93
Jose Leon <ljosecarlos295@gmail.com>
2022-02-26 22:29:15 -0500

    add configuration for client-angular, authorization2

--- src/main/java/com/dev/spring/boot/backend/apirest/auth/AuthorizationServerConfig.java
+++ src/main/java/com/dev/spring/boot/backend/apirest/auth/AuthorizationServerConfig.java
@@ -2,6 +2,7 @@ package com.dev.spring.boot.backend.apirest.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -25,17 +28,52 @@ class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		super.configure(security);
+		
+		security.tokenKeyAccess("permit_all()")
+		.checkTokenAccess("isAuthenticated()");
 	}
-
+	
+	/**
+	 * Configuracion de los clientes que se conectaran a la API rest
+	 * en este caso tenemos de ejemplo a la aplicacion ANGULAR
+	 */
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {		
-		super.configure(clients);
+		clients.inMemory().withClient("angularapp")
+		.secret(passwordEncoder.encode("12345"))
+		.scopes("read", "write")
+		.authorizedGrantTypes("password", "refresh_token")
+		.accessTokenValiditySeconds(3600)
+		.refreshTokenValiditySeconds(3600);
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		super.configure(endpoints);
+		/**
+		 *	accessTokenConverter: Se encarga de decodificar la informacion para que el 
+		 *  autthentication manager mediante OAuth2 pueda autenticar.
+		 *  En resumen traduce el token de acceso
+		 */
+		endpoints.authenticationManager(authenticationManager)
+		.tokenStore(tokenStore())
+		.accessTokenConverter(accessTokenConverter());
+	}
+	
+	
+	/**
+	 * Esto es usado en el metodo anterior pero se puede omitir ya que viene configurado
+	 * dentro de la clase AuthorizationServerEndpointsConfigurer
+	 */
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+
+		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		return jwtAccessTokenConverter;
 	}
 	
 	
